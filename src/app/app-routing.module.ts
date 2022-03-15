6436caaccc6634a67a0726385aff68b104589ba8
Aditya Pydimarri <aditya.pydimarri@gmail.com>
2022-03-14 20:02:55 -0500

    Cleaned and fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -15,14 +13,6 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
-  },
-  {
-    path: 'about',
-    component: AboutComponent
-  },
-  {
-    path: 'contact',
-    component: ContactComponent
   },
   {
     path: 'forgot-password',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { SignupComponent } from './signup/signup.component';
@@ -19,8 +17,6 @@ import { FormsModule } from '@angular/forms';
     LoginComponent,
     HomeComponent,
     NavbarComponent,
-    AboutComponent,
-    ContactComponent,
     FooterComponent,
     ForgotPasswordComponent,
     SignupComponent,
